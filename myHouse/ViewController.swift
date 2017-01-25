5df7bc805349fc415bd5f472ce77f945b7e87591
Valera Petin <valerapetin@macbook-air-valera.local>
2017-01-25 03:31:16 +0200

    Initial Commit

---
+++ myHouse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myHouse
+//
+//  Created by Valera Petin on 25.01.17.
+//  Copyright © 2017 Valery Petin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
